25cc45dfc36284d35ad040f6ea9ecb9811014e35
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-14 19:38:25 +0900

    To learn "Column"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
       title: 'Column & Row',
       home: Container(
         child: Center(
-          child: Row(
+          child: Column(
             children: <Widget>[
-              Container(color: Colors.blue, width: 100, height: 100),
-              Container(color: Colors.red[300], width: 100, height: 100),
+              Container( color: Colors.blue[300], width: 100, height: 100),
+              Container( color: Colors.red, width: 100, height: 100),
             ],
           ),
         ),
