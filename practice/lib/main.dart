a31db4b8798fcfe94a0827d5e70d0f2f702da18b
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-14 20:15:10 +0900

    To learn mainAxisAlignment of Column ver

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Column & Row',
       home: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,  // center, end
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Container( color: Colors.blue, width: 100, height: 100),
             Container( color: Colors.red, width: 100, height: 100),
@@ -20,5 +20,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+
   }
 }
