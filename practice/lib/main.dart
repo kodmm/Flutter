65107ace32e2d6731b4ca29ce35db2de8ae9d054
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-14 20:09:44 +0900

    To learn mainAxisAlignment of Row ver

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -12,21 +12,12 @@ class MyApp extends StatelessWidget {
       title: 'Column & Row',
       home: Container(
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,  // center, end
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container( color: Colors.blue, width: 100, height: 100),
-                Container( color: Colors.red, width: 100, height: 100),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Container( color: Colors.green, width: 100, height: 100),
-                Container(color: Colors.orange, width: 100, height: 100),
-              ]
-            )
+            Container( color: Colors.blue, width: 100, height: 100),
+            Container( color: Colors.red, width: 100, height: 100),
           ],
-        )
+        ),
       ),
     );
   }
