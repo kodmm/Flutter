8ca2e4c23fbbcf7a786057a7a6c8c58d4f2c9a0e
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-14 20:21:30 +0900

    To learn crossAxisAlignment of Row ver

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Column & Row',
       home: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container( color: Colors.blue, width: 100, height: 100),
             Container( color: Colors.red, width: 100, height: 100),
