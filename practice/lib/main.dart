58acf4a4f8f4cd664905113d99a9ddf0c32a460d
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-14 20:46:25 +0900

    To learn Main AxisAlignment of Space

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -9,18 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Column & Row',
-      home: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Container( color: Colors.blue, width: 100, height: 100),
-            Container( color: Colors.red, width: 100, height: 100),
-          ],
+      title: 'Alignment',
+      home: Center(
+        child: Container(
+          color: Colors.white,
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container( color: Colors.blue, width: 50, height: 50),
+              Container( color: Colors.red, width: 50, height: 50),
+              Container( color: Colors.green, width: 50, height: 50),
+              Container( color: Colors.orange, width: 50, height: 50),
+            ],
+          ),
         ),
       ),
     );
-
   }
 }
