19ee75707ad3fc7478a78268a7fd7e7e0ecf038d
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-19 14:33:45 +0900

    To learn "ListView.separated"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -8,44 +8,53 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ",];
+    var list = ["メッセージ","メッセージ", "メッセージ", "メッセージ", "メッセージ"];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('List view'),
+          title: Text('ListView'),
         ),
-        body: ListView.builder(
+        body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
-            if (index >= list.length) {
-              list.addAll(["メッセージ", "メッセージ", "メッセージ", "メッセージ",]);
-            }
             return _messageItem(list[index]);
           },
+          separatorBuilder: (BuildContext context, int index) {
+            return separatorItem();
+          },
+          itemCount: list.length,
         )
       )
     );
   }
+
+  Widget separatorItem() {
+    return Container(
+      height: 10,
+      color: Colors.orange,
+    );
+  }
+
+  Widget _messageItem(String title) {
+    return Container(
+      decoration: new BoxDecoration(
+        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+      ),
+      child:ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18.0
+          ),
+        ),
+        onTap: () {
+          print("onTap called.");
+        },
+        onLongPress: () {
+          print("onLongTap called.");
+        },
+      ),
+    );
+  }
 }
 
-Widget _messageItem(String title) {
-  return Container(
-    decoration: new BoxDecoration(
-      border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
-    ),
-    child:ListTile(
-      title: Text(
-        title,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18.0
-        ),
-      ),
-      onTap: (){
-        print("onTap called.");
-      },
-      onLongPress:() {
-        print("onLongTap called.");
-      },
-    ),
-  );
-}
