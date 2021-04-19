9ca94fdc7aaec6299a5efe26445f1482e5b8fa00
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-19 12:23:45 +0900

    To learn "ListView(children: []), _menuItem"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -8,14 +8,50 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const data = [Text("item0"), Text("item1"), Text("item2"), Text("item3"),
-      Text("item4")];
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
         body: ListView(
-          children: data
+          children: [
+            _menuItem("menu1", Icon(Icons.settings)),
+            _menuItem("menu2", Icon(Icons.map)),
+            _menuItem("menu3", Icon(Icons.room)),
+            _menuItem("menu4", Icon(Icons.local_shipping)),
+            _menuItem("menu5", Icon(Icons.airplanemode_active)),
+          ]
         ),
       ),
+    );
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return GestureDetector(
+      child:Container(
+        padding: EdgeInsets.all(8.0),
+        decoration: new BoxDecoration(
+          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child:icon,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                color:Colors.black,
+                fontSize: 18.0
+              ),
+            ),
+          ],
+        )
+      ),
+      onTap: () {
+        print("onTap called.");
+      },
     );
   }
 }
