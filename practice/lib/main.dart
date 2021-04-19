634eea9278973699b558f040add9b24b7c5ce4c3
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-19 23:39:08 +0900

    To learn "Stack SizedBox"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -14,17 +14,35 @@ class MyApp extends StatelessWidget {
           title: Text('Stack'),
         ),
         body:Stack(
+          alignment: Alignment.bottomRight,
           children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.green
+            SizedBox(
+              width: 400.0,
+              height: 400.0,
+              child: Container(color: Colors.orange),
             ),
-            Container(
-              width: 50,
-              height: 80,
-              color: Colors.orange
-            )
+            Positioned(
+              left: 20.0,
+              top: 20.0,
+              width: 300.0,
+              height: 300.0,
+              child: Container(color: Colors.blue),
+            ),
+            Positioned(
+              left: 10.0,
+              top: 10.0,
+              width: 100.0,
+              height: 100.0,
+              child: Container(color: Colors.green),
+            ),
+            Positioned(
+              left: 120.0,
+              top: 120.0,
+              width: 100.0,
+              height: 100.0,
+              child: Container(color: Colors.orange),
+            ),
+            Text('text')
           ],
         )
       )
