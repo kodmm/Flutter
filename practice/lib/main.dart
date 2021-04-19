4d87d3ec5ee55b280def4f4bdc9c5d349b236f19
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-19 13:44:46 +0900

    To learn "ListTile, leading title"

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -27,31 +27,26 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _menuItem(String title, Icon icon) {
-    return GestureDetector(
-      child:Container(
-        padding: EdgeInsets.all(8.0),
-        decoration: new BoxDecoration(
-          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
-        ),
-        child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child:icon,
-            ),
-            Text(
-              title,
-              style: TextStyle(
-                color:Colors.black,
-                fontSize: 18.0
-              ),
-            ),
-          ],
-        )
+    return Container(
+      decoration: new BoxDecoration(
+        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
       ),
-      onTap: () {
-        print("onTap called.");
-      },
+      child:ListTile(
+        leading: icon,
+        title: Text(
+          title,
+          style: TextStyle(
+            color:Colors.black,
+            fontSize: 18.0
+          ),
+        ),
+        onTap:(){
+          print("onTap called.");
+        },
+        onLongPress: (){
+          print("onLongPress called.");
+        },
+      ),
     );
   }
 }
