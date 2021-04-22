8baed27d1e15dcf450cb19d308c4c5d17fcf0c45
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-23 08:22:27 +0900

    Create "Add List "

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -69,12 +69,38 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面(クリックで戻る)'),
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // テキストを入力
+            TextField(),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+
+                onPressed: (){},
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+
+          ],
         ),
       ),
     );
