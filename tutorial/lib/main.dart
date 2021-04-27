e12d505ca8648eb462170df5a8c34bc0632097d9
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-27 10:03:30 +0900

    feature SignUp, SignIn From Firebase-auth

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter/rendering.dart';
-void main() {
-  debugPaintSizeEnabled = true;
+Future<void> main() async {
+  // Firebase初期化
+
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,53 +12,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords()
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyAuthPage(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class MyAuthPage extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  _MyAuthPageState createState() => _MyAuthPageState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class _MyAuthPageState extends State<MyAuthPage> {
+
+  // 登録
+  String newUserEmail = '';
+  String newUserPassword = '';
+
+  // ログイン
+  String loginUserEmail = '';
+  String loginUserPassword = '';
+
+  // 登録・ログインに関する情報を表示
+  String infoText = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                // テキスト入力のラベルを設定
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState((){
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード(6文字以上)"),
+                // パスワードが見えないようにする
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      // mail / password でユーザ登録
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final UserCredential result =
+
+                      await auth.createUserWithEmailAndPassword(
+                          email: newUserEmail,
+                          password: newUserPassword
+                      );
+
+
+                      // 登録したUser info
+                      final User user = result.user;
+                      setState(() {
+                        infoText = "登録OK:${user.email}";
+                      });
+                    } catch(e) {
+                      setState(() {
+                        infoText = "登録NG:${e.toString()}";
+                      });
+                    }
+                  },
+                  child: Text("ユーザ登録")
+              ),
+              const SizedBox(height: 32),
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState((){
+                    loginUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState((){
+                    loginUserPassword = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                        await auth.signInWithEmailAndPassword(
+                          email: loginUserEmail,
+                          password: loginUserPassword,
+                        );
+                    final User user = result.user;
+                    setState(() {
+                      infoText = "ログインOK: ${user.email}";
+                    });
+                  } catch(e) {
+                    setState((){
+                      infoText = "ログインNG:${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("ログイン"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText),
+            ],
+          ),
+
+        ),
       ),
-      body: _buildSuggestions(),
     );
   }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-
 }
 
