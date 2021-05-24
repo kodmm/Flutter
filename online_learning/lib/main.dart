86218b88a5fe9d3565fa0641f363fa9d5d64ae3e
kodmm <53547701+kodmm@users.noreply.github.com>
2021-05-24 19:25:38 +0900

    コース一覧Page

--- online_learning/lib/main.dart
+++ online_learning/lib/main.dart
@@ -253,6 +253,8 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage>  {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -272,8 +274,140 @@ class _TopPageState extends State<TopPage>  {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          Navigator.of(context)
+              .push(
+            PageRouteBuilder(pageBuilder: (_, __, ____) => CoursePage()),
+          );
+        },
         child: Icon(Icons.list),
+      ),
+    );
+  }
+}
+
+
+class _CourseCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String logoUrl;
+
+  const _CourseCard({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.logoUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        child: ListTile(
+          leading: Container(
+            width: 48,
+            height: 48,
+            padding: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Image.network(
+              logoUrl,
+              fit: BoxFit.contain,
+            ),
+          ),
+          title: Text(title),
+          subtitle: Text(subtitle),
+          trailing: Icon(Icons.more_horiz),
+        ),
+      ),
+    );
+  }
+}
+
+class _Recommended extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 32, bottom: 8, left: 8),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Recommended',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        _CourseCard(
+          title: 'Figma',
+          subtitle: 'Figma Mastery',
+          logoUrl: figmaLogoUrl,
+        ),
+        _CourseCard(
+          title: 'Sketch',
+          subtitle: 'Symbol Libraries',
+          logoUrl: sketchLogoUrl,
+        ),
+        _CourseCard(
+          title: 'Adobe XD',
+          subtitle: 'Fundamentals of Xd',
+          logoUrl: xdLogoUrl,
+        ),
+        _CourseCard(
+          title: 'Figma',
+          subtitle: 'Figma Mastery',
+          logoUrl: figmaLogoUrl,
+        ),
+        _CourseCard(
+          title: 'Sketch',
+          subtitle: 'Symbol Libraries',
+          logoUrl: sketchLogoUrl,
+        ),
+        _CourseCard(
+          title: 'Adobe XD',
+          subtitle: 'Fundamentals of Xd',
+          logoUrl: xdLogoUrl,
+        ),
+      ],
+    );
+  }
+}
+
+class CoursePage extends StatefulWidget {
+  @override
+  _CoursePageState createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  _Header(title: 'Course'),
+                  _Recommended(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Icon(Icons.keyboard_backspace),
       ),
     );
   }
